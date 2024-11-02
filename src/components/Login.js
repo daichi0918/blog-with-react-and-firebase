2a00da20c87cdb727dcfd3f376b01baf0b21f130
daichi0918 <kayasugadaichi@icloud.com>
2024-11-02 15:34:46 +0900

    finishi:chapter31

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  const loginInWithGoogle = () => {
+    // Googleでログイン
+  };
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
+  );
 };
 
 export default Login;
