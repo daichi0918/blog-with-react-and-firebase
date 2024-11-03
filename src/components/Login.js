92aa8bd2d21026874f57bae3e10fab9934feee90
daichi0918 <kayasugadaichi@icloud.com>
2024-11-03 10:35:22 +0900

    修正

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,11 @@
+import { signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth, provider } from '../firebase';
 
 const Login = () => {
   const loginInWithGoogle = () => {
     // Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {});
   };
   return (
     <div>
